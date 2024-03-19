e06f5e0df4659a6379e13914a0970efbb349185d
Saul Gomes <saul.contatodev@gmail.com>
2024-03-18 23:56:22 -0300

    feat(migrations): create user table

---
+++ migrations/0001_users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(150) NOT NULL,
+    name VARCHAR(180) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    is_adm BOOLEAN DEFAULT FALSE
+);
+
+CREATE INDEX idx_id ON users (id);
